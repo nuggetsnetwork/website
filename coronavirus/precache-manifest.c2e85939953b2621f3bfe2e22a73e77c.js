c5f97b5013addca180ff021a41c1c57139cac54c
nuggetsnetwork <64025145+nuggetsnetwork@users.noreply.github.com>
2020-04-26 18:35:48 +0530

    Moving corona project
    coronaproject

---
+++ coronavirus/precache-manifest.c2e85939953b2621f3bfe2e22a73e77c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b638a85ba1d08fe1637b6944970c58d3",
+    "url": "/coronavirus/index.html"
+  },
+  {
+    "revision": "a088c56d7203b91312ed",
+    "url": "/coronavirus/static/css/main.4f662c05.chunk.css"
+  },
+  {
+    "revision": "ec4428319cb6902bb7f5",
+    "url": "/coronavirus/static/js/2.8d066c97.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/coronavirus/static/js/2.8d066c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a088c56d7203b91312ed",
+    "url": "/coronavirus/static/js/main.9e687d01.chunk.js"
+  },
+  {
+    "revision": "eec4c43958551acbf4d3",
+    "url": "/coronavirus/static/js/runtime-main.f9f4b5c9.js"
+  }
+]);
