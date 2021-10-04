bf51150d7fb878c2f7204883594458a1d649edfc
nuggetsnetwork <64025145+nuggetsnetwork@users.noreply.github.com>
2021-10-04 17:16:30 +0530

    Changes HTML to Firebase

--- UnoCog/version2/firebaseWebLogin/index.js
+++ UnoCog/version2/firebaseWebLogin/index.js
@@ -8,9 +8,18 @@ firebase.auth().onAuthStateChanged(function(user) {
     var user = firebase.auth().currentUser;
 
     if(user != null){
-
-      var email_id = user.email;
-      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+      var isEmailVerified = user.emailVerified;
+      if (isEmailVerified){
+        var email_id = user.email;
+        document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+      }
+        else {
+          document.getElementById("user_div").style.display = "none";
+          document.getElementById("login_div").style.display = "block";
+          document.getElementById("error").innerHTML = "Email verification pending!";
+        }
+      
+      
 
     }
 
@@ -27,18 +36,69 @@ function login(){
 
   var userEmail = document.getElementById("email_field").value;
   var userPass = document.getElementById("password_field").value;
-
-  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+  
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass)
+  // .then((userCredential) => {
+  //   // Signed in 
+  //   var user = userCredential.user;
+  //   // ...
+  //   // var user = firebase.auth().currentUser;
+  // })
+  
+  
+  .catch(function(error) {
     // Handle Errors here.
+    
     var errorCode = error.code;
     var errorMessage = error.message;
-
-    window.alert("Error : " + errorMessage);
+    document.getElementById("error").innerHTML = error.message
+    //window.alert("Error : " + errorMessage);
 
     // ...
   });
-
 }
+
+  function signup(){
+
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass)
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    // ...
+    verifyEmail()
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    document.getElementById("error").innerHTML = error.message
+    // ..
+  });
+
+  }
+
+  function forgotPass(){
+    var userEmail = document.getElementById("email_field").value;
+    firebase.auth().sendPasswordResetEmail(userEmail)
+  .then(() => {
+    alert("Password reset email sent!")
+    // ..
+  })
+  .catch((error) => {
+    document.getElementById("error").innerHTML = error.message
+  });
+  }
+
+  function verifyEmail(){
+    firebase.auth().currentUser.sendEmailVerification()
+    .then(() => {
+      document.getElementById("error").innerHTML = "Verification email sent. click on verify link and login"
+    });
+
+  }
+  
 
 function logout(){
   firebase.auth().signOut();
---
+++ UnoCog/version2/firebaseWebLogin/test.js
@@ -0,0 +1,41 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-analytics.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC0ErckrTXfy_QmdoUasw28H4ib38qhh_o",
+  authDomain: "nuggetsnetwork-2021.firebaseapp.com",
+  databaseURL: "https://nuggetsnetwork-2021-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "nuggetsnetwork-2021",
+  storageBucket: "nuggetsnetwork-2021.appspot.com",
+  messagingSenderId: "782216336589",
+  appId: "1:782216336589:web:fd24eb6e6b831032014584",
+  measurementId: "G-N53KN5RYZL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app)
+
+// import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+// const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.email;
+    console.log("User Logged In.")
+    console.log(uid)
+    // ...
+  } else {
+    // User is signed out
+    // ...
+    console.log("No User Logged In.")
+  }
+});
