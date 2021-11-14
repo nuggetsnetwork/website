6dc5e41bc9e3eadd659833d81137517a254396cd
nuggetsnetwork <64025145+nuggetsnetwork@users.noreply.github.com>
2021-11-14 12:20:39 +0530

    Movies UI
    ReactJS new UI test

---
+++ Products/moviesui/precache-manifest.6e124f916a55aecebfa68a7c1e8c97cb.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = [
+  {
+    "revision": "5131620a02f0104516fa",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/0.db417712.chunk.js"
+  },
+  {
+    "revision": "eaf15019b683487e1c00",
+    "url": "https://reduction-admin.github.io/react-reduction/static/css/main.2aa558f5.chunk.css"
+  },
+  {
+    "revision": "eaf15019b683487e1c00",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/main.56157d2c.chunk.js"
+  },
+  {
+    "revision": "43cf907c79aa133812af",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/runtime~main.befee590.js"
+  },
+  {
+    "revision": "8e39537e5ca38452ee46",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/3.cdf64bd4.chunk.js"
+  },
+  {
+    "revision": "96bfbb2fe687c9348172",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/4.7233748e.chunk.js"
+  },
+  {
+    "revision": "05c3474f197cce14cf0f",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/5.bacf553d.chunk.js"
+  },
+  {
+    "revision": "402bbecedcbc686e3c84",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/6.64ef9f1d.chunk.js"
+  },
+  {
+    "revision": "1dfc98308fb62173553a",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/7.b0ae68c1.chunk.js"
+  },
+  {
+    "revision": "7eed2a4ec2c33c34a649",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/8.ac052c4e.chunk.js"
+  },
+  {
+    "revision": "ecc8392df3bc47d30a0c",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/9.6ec14c5a.chunk.js"
+  },
+  {
+    "revision": "6c02407a0eacce8c3236",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/10.2216581f.chunk.js"
+  },
+  {
+    "revision": "b6e40518c4cac156c50d",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/11.4458b05c.chunk.js"
+  },
+  {
+    "revision": "ad1a63a70708a5a87a61",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/12.744984b1.chunk.js"
+  },
+  {
+    "revision": "9d61d69bf0f32d621dff",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/13.4f0d54eb.chunk.js"
+  },
+  {
+    "revision": "b17f7e0f8f488820be17",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/14.9917a620.chunk.js"
+  },
+  {
+    "revision": "56fe5c0088c2883767a9",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/15.a88a8230.chunk.js"
+  },
+  {
+    "revision": "f0a1779a210b8d415979",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/16.21e1832a.chunk.js"
+  },
+  {
+    "revision": "dcf4b9f210c0a32ba90c",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/17.d02e7103.chunk.js"
+  },
+  {
+    "revision": "c9f5e7aff087ba4d3ea5",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/18.72e15b99.chunk.js"
+  },
+  {
+    "revision": "0ecab689dd9894d41138",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/19.9b8171c6.chunk.js"
+  },
+  {
+    "revision": "def43293412f9dc4ca41",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/20.54015500.chunk.js"
+  },
+  {
+    "revision": "978e51b554db00a67e2796661b3526fe",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/100_4.978e51b5.jpg"
+  },
+  {
+    "revision": "b175e1c4790112fe609a0f3279e8535d",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/logo_200.b175e1c4.png"
+  },
+  {
+    "revision": "80d4a4e5af9b6cf3882e4a34c5497542",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/sidebar-4.80d4a4e5.jpg"
+  },
+  {
+    "revision": "1f7beca5bf4cda1803cd59cd881c372d",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/100_1.1f7beca5.jpg"
+  },
+  {
+    "revision": "82e7c41e073394f808fd382c97e71884",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/100_2.82e7c41e.jpg"
+  },
+  {
+    "revision": "6e25d86d33b97ab231889fd4a38530b4",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/100_3.6e25d86d.jpg"
+  },
+  {
+    "revision": "fd53372585bf0ef377e97ee9dd7ea925",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/100_5.fd533725.jpg"
+  },
+  {
+    "revision": "c54eef50718560d1268aa8e92025972d",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/background_1920-2.c54eef50.jpg"
+  },
+  {
+    "revision": "b7f68e8afaec5bfad8fdcc44e4e4e483",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/background_1920-11.b7f68e8a.jpg"
+  },
+  {
+    "revision": "acfe7046f47d96b9cb598622e50fe610",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/background_1920-18.acfe7046.jpg"
+  },
+  {
+    "revision": "1420d15b22ed3f39abc468212d735814",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/background_640-1.1420d15b.jpg"
+  },
+  {
+    "revision": "55e47e98ef3c84cf5eb4706a4c74311a",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/background_640-3.55e47e98.jpg"
+  },
+  {
+    "revision": "1d98ff2e44d70e391ec7a49e4efdfa86",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/background_1920-5.1d98ff2e.jpg"
+  },
+  {
+    "revision": "1e0eb5503400ca7f2a65175cec9e9115",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/background_1920-6.1e0eb550.jpg"
+  },
+  {
+    "revision": "abf47fc19906e17185a37765e7594ee0",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/background_1920-7.abf47fc1.png"
+  },
+  {
+    "revision": "22fc19f2c02f7dba20864f097f0bfd20",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/background_1920-19.22fc19f2.jpg"
+  },
+  {
+    "revision": "a9a0ec6482226ea8098fc71a3fababe0",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/product_640-1.a9a0ec64.jpg"
+  },
+  {
+    "revision": "da0b0457353c6555a5ba4535597f2187",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/product_640-2.da0b0457.jpg"
+  },
+  {
+    "revision": "64ed27c9aca0306b4185b82e144cdedd",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/product_640-3.64ed27c9.jpg"
+  },
+  {
+    "revision": "2110d8cd7d70f6792b4c2d517cb7f593",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/product_640-4.2110d8cd.jpg"
+  },
+  {
+    "revision": "bf8840bd88a80c14418b238a7f1beb24",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/product_640-5.bf8840bd.jpg"
+  },
+  {
+    "revision": "f9249755162af20121af258ca1ffc578",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/product_640-6.f9249755.jpg"
+  },
+  {
+    "revision": "de88253696b81598494dd6bd5c0b087e",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/100_6.de882536.jpg"
+  },
+  {
+    "revision": "1dce100ffdb944226acf9db666a57319",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/100_7.1dce100f.jpg"
+  },
+  {
+    "revision": "a4be0bbf3a08f9de24548aa6765af93f",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/100_8.a4be0bbf.jpg"
+  },
+  {
+    "revision": "798c02eb53d02e11c3d8a6588cdf5033",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/100_9.798c02eb.jpg"
+  },
+  {
+    "revision": "56d054ffdf15ac516d7ba19fa9a33ad5",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/100_10.56d054ff.jpg"
+  },
+  {
+    "revision": "336faadc989ea15bbe2c0fb30e20bcd7",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/100_11.336faadc.jpg"
+  },
+  {
+    "revision": "1e0889afa5d1fa653fef4f8b5d3272d2",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/100_12.1e0889af.jpg"
+  },
+  {
+    "revision": "63265e929c9f7442ea1853d2dc19a188",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/100_13.63265e92.jpg"
+  },
+  {
+    "revision": "595b8cbdd2b7f27cf7d38b895a9b56c9",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/100_14.595b8cbd.jpg"
+  },
+  {
+    "revision": "2f5e6c7ded42484cad62bcea5fe8db2d",
+    "url": "https://reduction-admin.github.io/react-reduction/index.html"
+  }
+];
